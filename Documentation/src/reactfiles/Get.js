f7d01a58d2d8f23403c1039854a8773b892aad5a
Feranmi <feranmi.falade@ucalgary.ca>
2023-11-11 16:45:02 -0700

    get ben

--- Documentation/src/reactfiles/Get.js
+++ Documentation/src/reactfiles/Get.js
@@ -9,15 +9,19 @@ export default function Get() {
           <h2>Documentation for <i>get</i> Method in Connection Class</h2>
           <br />
           <h3>Method Signature</h3>
+          <br />
           <code>def get(self, key)</code>
           <br />
           <h3>Parameters</h3>
+          <br />
           <l>key: A string representing the name of the table to be retrieved from the server.</l>
           <br />
           <h3>Description</h3>
+          <br />
           <p>The get method in the Connection class is designed to retrieve a specific table from the server. It takes a single parameter, key, which specifies the name of the table to be fetched.</p>
           <br />
           <h3>Usage</h3>
+          <br />
           <p>Initiating Connection: The method begins by establishing a connection to the server using the connect method.</p>
           <p>Local Search: It checks if the table with the specified key already exists in the local tables array. If found, it returns this table immediately, avoiding a network request.</p>
           <p>Request Preparation: If the table is not found locally, the method prepares a JSON object containing the request information. This includes the method type ("get"), user credentials (username, accountkey), and the tableName which is the key parameter.</p>
@@ -27,9 +31,22 @@ export default function Get() {
           <p>Closing Connection: The connection to the server is closed, and the new Table object is returned.</p>
           <br />
           <h3>Return Value</h3>
-          <l>Returns an instance of the Table class corresponding to the requested table name.</l>
+          <br />
+          <li>Returns an instance of the Table class corresponding to the requested table name.</li>
           <br />
           <h3>Example</h3>
+          <br />
+          <code>connection = Connection("192.168.1.1", "username", "password")</code>
+          <code>my_table = connection.get("employees")</code>
+          <br />
+          <p>In this example, a Connection object is first created with the server IP address, username, and password. Then, the get method is called with the key "employees", which fetches the "employees" table from the server.</p>
+          <br />
+          <h3>Important Notes</h3>
+          <br />
+          <li>This method automatically handles the connection to the server, including opening and closing the connection.</li>
+          <li>It's assumed that the server responds with the appropriate data in a format that the Table constructor can process.</li>
+          <li>Error handling (for instance, for connection failures or if the server returns an error response) is not explicitly implemented in this method and should be considered for robustness.</li>
+          <li>The method destructs the Connection object at the end, which might not be ideal in scenarios where multiple requests are made in succession. Consider managing the connection lifecycle outside of individual method calls for efficiency.</li>
         </div>
       </div>
     </div>
