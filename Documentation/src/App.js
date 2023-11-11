7671161017c4ec8d81938f238e353cc7ef57e90e
Feranmi <feranmi.falade@ucalgary.ca>
2023-11-11 15:22:27 -0700

    ben

--- Documentation/src/App.js
+++ Documentation/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
-import View from "./reactfiles/View"
-import Edit from "./reactfiles/Edit"
+import Get from "./reactfiles/Get"
+import Set from "./reactfiles/Set"
+import Delete from "./reactfiles/Delete"
 
 function App() {
   const notes = [
-    { id: 1, title: "Note 1", content: "This is the first note" },
-    { id: 2, title: "Note 2", content: "This is the second note" },
-    { id: 3, title: "Note 3", content: "This is the third note" }
+    { id: 1, title: "Get", content: "This is how to GET information" },
+    { id: 2, title: "Set", content: "This is how to SET information" },
+    { id: 3, title: "Delete", content: "This is how to DELETE information" }
   ];
   
   return (
@@ -16,9 +17,10 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/"></Route>
-          <Route path="/notes" element={<div className="no-note-selected">Select a note, or create a new one.</div>}></Route>
-          <Route path="/notes/:noteID" element={<View notes={notes} />}></Route>
-          <Route path="/notes/:noteID/edit" element={<Edit notes={notes} />}></Route>
+          <Route path="/info" element={<div className="no-note-selected">Heyyyyyyy</div>}></Route>
+          <Route path="/info/get" element={<Get />}></Route>
+          <Route path="/info/set" element={<Set />}></Route>
+          <Route path="/info/delete" element={<Delete />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ Documentation/src/reactfiles/Set.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Set() {
+
+  return (
+    <div id="Note">
+      <div className="note-info">
+        <div className="title-date">
+          <h2>How to SET information:</h2>
+          <small>In order to retrieve info, you need to initialize a connection object. Do this by calling connection.set and the argument is a key, which is the key of the table</small>
+        </div>
+      </div>
+    </div>
+  );
+}
