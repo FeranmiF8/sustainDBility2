f1869dbe22f75fdf6b32b59216d624cb01036b3f
TheKenDoll <58020437+TheKenDoll@users.noreply.github.com>
2023-11-11 10:58:20 -0700

    added template func calls

--- Server/main.py
+++ Server/main.py
@@ -10,3 +10,24 @@ df = pd.DataFrame(data)
 df.to_csv("test.csv")
 
 print(df)
+
+# {
+#     "method":"get/set/delete",
+#     "username":"kenzie",
+#     "accountkey":"poop",
+#     "tableName":"table1",
+#     "data":"whatever"
+# }
+
+
+# get func call
+def getCall():
+    return
+
+# set func call
+def setCall():
+    return
+
+# delete func call
+def deleteCall():
+    return
