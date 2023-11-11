c33b88a5edb9e88487d48707b8fd4abe1bd33198
1saacH <isaacmhus@gmail.com>
2023-11-11 11:23:15 -0700

    updated table

--- Library/table.py
+++ Library/table.py
@@ -5,10 +5,10 @@ class Table:
     data = []
     server = None
     
-    def __init__(self, name, server):
+    def __init__(self, name, data):
         self.name = name
-        self.data = []
-        #data = server.getTable(name)
+        self.data = data
+        print('table object created')
         
     def get(self, key):
         for i in self.data:
