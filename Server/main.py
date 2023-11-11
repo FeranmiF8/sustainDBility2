8da7b99a58e5f594f507a2c48a4055f26c6a64ab
TheKenDoll <58020437+TheKenDoll@users.noreply.github.com>
2023-11-11 11:42:17 -0700

    added basic setCall functionality

--- Server/main.py
+++ Server/main.py
@@ -1,33 +1,43 @@
 import pandas as pd
 
-data = {
-    "Temperature":[12, 43, 35],
-    "Moisture":[65, 43, 32]
-}
-
-df = pd.DataFrame(data)
-
-df.to_csv("test.csv")
-
-print(df)
-
-# {
-#     "method":"get/set/delete",
-#     "username":"kenzie",
-#     "accountkey":"poop",
-#     "tableName":"table1",
-#     "data":"whatever"
+# data = {
+#     "Temperature":[12, 43, 35],
+#     "Moisture":[65, 43, 32]
 # }
 
+# df = pd.DataFrame(data)
+
+# df.to_csv("test.csv")
+
+# print(df)
+
+datajson = {
+    "method":"set",
+    "username":"kenzie",
+    "accountkey":"poop",
+    "tableName":"table1",
+    "data":{
+        "Temperature":[18, 43, 35],
+        "Moisture":[65, 43, 32]
+    }
+}
+
+print(datajson["method"])
 
 # get func call
 def getCall():
     return
 
 # set func call
-def setCall():
-    return
+def setCall(username, key, table, data):
+    df = pd.DataFrame(data)
+    df.to_csv(f"./{username}_{key}/{table}.csv")
 
 # delete func call
 def deleteCall():
     return
+
+if datajson["method"] == "get":
+    getCall()
+elif datajson["method"] == "set":
+    setCall(datajson["username"], datajson["accountkey"], datajson["tableName"], datajson["data"])
